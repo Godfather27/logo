47a3b6ab4ea6e53070130ad1249d7af2eb5c8e1e
Sebastian Huber <sebastian_christoph_huber@yahoo.de>
2018-11-30 13:21:22 +0100

    feat: tokenizer

--- js/tokenizer.js
+++ js/tokenizer.js
@@ -2,6 +2,11 @@ class Token {
   constructor(type, data) {
     this.type = type;
     this.data = data;
+    this.args = [];
+  }
+
+  addArg(arg) {
+    this.args.push(arg)
   }
 
   toString() {
@@ -9,18 +14,55 @@ class Token {
   }
 }
 
+const ops = [ '!=', '<>', '<=', '>=', '<', '>', '+', '-', '*', '/', '%', '=', '[', ']', '(', ')' ];
 class Tokenizer {
   constructor() {
   }
 
   load(text) {
+    text = text
+    .replace(/\;.*$/g,"")
+    .replace(/\n/g,"")
+    .replace(/(\()([^\s])/g, (_, p1, p2) => `${p1} ${p2}`)
+    .replace(/([^\s])(\))/g, (_, p1, p2) => `${p1} ${p2}`)
+    console.log(text)
+    this.source = text
+      .split(" ")
   }
 
   peek() {
+    let data = this.source[0];
+    let type;
+    [data, type] = this.tokenize(data)
+    return new Token(type, data);
+  }
+
+  tokenize(data) {
+    let type;
+    if(typeof data === "undefined" || data === "") {
+      type = "eof";
+    } else if(!isNaN(parseFloat(data)) && typeof parseFloat(data) === "number") {
+      data = parseFloat(data);
+      type = "num";
+    } else if (/to|end/i.test(data) || ops.indexOf(data) !== -1) {
+      type = "ops"
+      data = data.toLowerCase();
+    } else if (/^:/.test(data)) {
+      type = "var"
+      data = data.substr(1);
+    } else if (/^"/.test(data)) {
+      type = "sym"
+      data = data.substr(1);
+    } else {
+      type = "wrd";
+    }
+    return [data, type]
   }
 
   next() {
-    return new Token('eof', '');
+    const [data, type] = this.tokenize(this.source.shift());
+    const args = [];
+    return new Token(type, data, args);
   }
 }
 
