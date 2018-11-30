03215f124e6a5bf41881e45b31e01481adfdffa0
Sebastian Huber <sebastian_christoph_huber@yahoo.de>
2018-11-30 13:21:36 +0100

    feat: parser basic

--- js/parser.js
+++ js/parser.js
@@ -1,1 +1,35 @@
-/* add clever code here */
+class Parser {
+  constructor() {
+    this.commands = new Map();
+    this.infix = new Map();
+    this.infix.set("(", Infinity);
+    this.stack = [];
+  }
+
+  load(tokenizer) {
+    this.tokenizer = tokenizer;
+  }
+
+  command(token) {
+    const argsCount = this.commands.get(token.data)
+    for(let i = 0; i < argsCount; i++)
+      token.addArg(this.tokenizer.next());
+    return token
+  }
+
+  next() {
+    let token = this.tokenizer.next();
+    if(this.commands.has(token.data)) {
+      token = this.command(token)
+    }
+    return token;
+  }
+
+  addCommand(command, argsLength) {
+    this.commands.set(command, argsLength);
+  }
+
+  addInfix(operator, precedent) {
+    this.infix.set(operator, precedent)
+  }
+}
